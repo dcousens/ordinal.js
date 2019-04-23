1986105a2a48d229ad625e0cefd2f01a27a99a75
Omri Lotan <omrilotan@users.noreply.github.com>
2019-04-23 17:46:02 +0100

    Compatibility for node < 6

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var specialNumbers = [Infinity, -Infinity, NaN, -NaN]
 function ordinal (i) {
   if (typeof i !== 'number') throw new TypeError('Expected Number, got ' + (typeof i) + ' ' + i)
 
-  if (specialNumbers.includes(i)) {
+  if (!!~specialNumbers.indexOf(i)) {
     return i
   }
   return i + indicator(i)
