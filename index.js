4e57b79d6d02a8e5b626afe61070b4556955f161
Omri Lotan <omrilotan@users.noreply.github.com>
2019-04-24 08:10:45 +0100

    replace bitwise not operator with netgative comparison

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var specialNumbers = [Infinity, -Infinity, NaN, -NaN]
 function ordinal (i) {
   if (typeof i !== 'number') throw new TypeError('Expected Number, got ' + (typeof i) + ' ' + i)
 
-  if (~specialNumbers.indexOf(i)) {
+  if (specialNumbers.indexOf(i) !== -1) {
     return i
   }
   return i + indicator(i)
