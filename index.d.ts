9d4de882db21d140fbfc9214ac977c804cb5bd92
Luke Bennett <lbennett.inbox@gmail.com>
2024-01-02 17:45:09 +0000

    feat: interpolated literal return types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 import indicator from './indicator'
 export { indicator }
 
-export default function ordinal (i: number): string
+export default function ordinal (i: number): `${string}${'th' | 'st' | 'nd' | 'rd'}`
